0e2d65e4f7bc84b21d7dc30ff2f4c2a5b3be94eb
Sumya <sumyahoque2012@gmail.com>
2022-09-05 14:54:12 -0400

    React app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {Components} from './components/Components'
+import NavBar from './components/navbar'
+import {Footer} from './components/Footer'
+//import { Routes, Route}  from 'react-router-dom'
+
+function App() {
+  return (
+ <div className="App">
+     <NavBar />
+   
+<Components/ >  
+  </div>
+    
+  );
+}
+
+export default App;
