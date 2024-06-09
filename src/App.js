3abd74c4820c9bd1c6259cb9e22cd2de8b884a05
Sumya <sumyahoque2012@gmail.com>
2024-06-09 01:18:27 -0400

    ad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import {Components} from './components/Components'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { Routes, Route}  from 'react-router-dom'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import { BrowserRouter, Routes, Route}  from 'react-router-dom'
+import {Nao} from './components/Nao_Project'
+import { Voting } from './components/Voting_project';
 
 function App() {
   return (
- <div className="App container-fluid">
-    <Components/ >  
+ <div className="App container-fluid">  
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/' element={<Components></Components>}></Route>
+        <Route path='/nao' element= {<Nao></Nao>}></Route>
+        <Route path='/voting_ledger' element ={<Voting></Voting>}></Route>
+        
+      </Routes>
+    </BrowserRouter>
+
   </div>
     
   );
