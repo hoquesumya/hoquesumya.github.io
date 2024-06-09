c9a93db8db125b257166e0bf9ea3f934d910c1f9
Sumya <sumyahoque2012@gmail.com>
2024-06-09 01:17:58 -0400

    added

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,52 +1,64 @@
-import React from 'react'
+import {React,useState} from 'react'
 import '../styles/Project.css'
-import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom';
 
 
 export const Project = (props)=>{
+
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const navigate = useNavigate()
+
+
     let list_of_project = [{
-        "image_name":"Mental Health Assistant",
-        "image_src":"sumya/assests/robot.png",
+        "image_name":"nao",
+        "image_src":"assests/robot.png",
         "tools":"Choreographe, Java, Node.js, MongoDB, Alexa"
     },
     {
-        "image_name":"Distributed voting ledger" ,
-        "image_src":"sumya/assests/voting.png",
+        "image_name":"voting_ledger" ,
+        "image_src":"assests/voting.png",
         "tools":"Python, Jquery, Blockchain"
     },
 
     {
-        "image_name":"Cobra",
-        "image_src":"sumya/assests/cobra.jpeg",
+        "image_name":"cobra",
+        "image_src":"assests/cobra.jpeg",
         "tools":"OCaml, LLVM"
     },
 
     {
-        "image_name":"Soccer_info",
-        "image_src":"sumya/assests/soccer.png",
+        "image_name":"soccer_info",
+        "image_src":"assests/soccer.png",
         "tools":"Python, JavaScript, Google Cloud, SQL"
     },
 
 
     {
-        "image_name":"My_pantry",
-        "image_src":"sumya/assests/mypantry.png",
+        "image_name":"my_pantry",
+        "image_src":"assests/mypantry.png",
         "tools":"C, Kernel API"
     }
 
 
     ]
 
-    let className = "button"
-    const handleMouseEnter = ()=>{
-        className = "button-temp"
+    
+    const handleMouseEnter = (index)=>{
+        setHoveredIndex(index)
 
     }
 
     const handleMouseLeave = () =>{
-        className = "button"
+        setHoveredIndex(null)
+    }
+
+    const handleClick = (project)=>{
+        console.log(project)
+        navigate(`/${project}`);
 
     }
+
+
     return (
 
         <div className = "project pt-5" id = "project-sec">
@@ -60,11 +72,29 @@ export const Project = (props)=>{
                 
                     return (
                         <div className="sub-branch" key={index}>
-                            <div className={`sub-project${index}`} onMouseEnter={handleMouseEnter} onMouseLeave ={handleMouseLeave}>
-                                <img className={`${item["image_name"]} img-fluid`} src={item["image_src"]} alt={`Project ${index}`} />
-                                <div className={className}>
-                                    <button type="button" className="btn btn-lg" >View Details</button>
+                            <div 
+                            className={`sub-project${index}`} 
+                            onMouseEnter={() => handleMouseEnter(index)} 
+                            onMouseLeave ={handleMouseLeave}
+                            >
+                                <img className={`${item["image_name"]} img-fluid`}
+                                 src={item["image_src"]}
+                                  alt={`Project ${index}`}
+                                   />
+                                {hoveredIndex === index && (
+                                <div className="button">
+                                    
+                                        <button type="button"
+                                        
+                                        className="btn btn-lg" 
+                                        onClick={()=>handleClick(item["image_name"])}
+                                        
+                                        >
+                                            View Details
+                                        </button>
+                                    
                                 </div>
+                                )}
 
 
                             </div>
